9271afe147b741d114755c83d6967bc0cabab85c
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-05-30 05:53:00 -0700

    done with task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_first_3 = "Hol"
 word_last_2 = "on"
-middle_word = "olberto"
+middle_word ="olberto"
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
