96bff8b91fe55b682a53f5f4673640317d057988
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-05-31 04:05:25 -0700

    made task 1 executable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ print('Last digit of', number, 'is', remainder, end=' ')
 if (number > 5):
     print('and is greater than 5')
 elif(number == 0):
-    print('and is zero')
+    print('and is 0')
 else:
     print('and is less than 6 and not 0')
