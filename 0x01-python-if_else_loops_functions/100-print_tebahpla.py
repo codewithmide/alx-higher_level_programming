0b3db5366e7e53d68a5385d10e72c38765fddc9a
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-05-31 12:12:02 -0700

    used the pycodestyle

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') -1, -2):
-    print("{:c}{:s}".format(i,chr(i - 33)), end="")
+for i in range(ord('z'), ord('a') - 1, -2):
+    print("{:c}{:s}".format(i, chr(i - 33)), end="")
