a6f0c2c3959da5f96c061646079dae78b51d1e1a
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-05-31 04:02:08 -0700

    created task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if (number > 0):
+    remainder = number % 10
+else:
+    remainder = number % -10
+print('Last digit of', number, 'is', remainder, end=' ')
+if (number > 5):
+    print('and is greater than 5')
+elif(number == 0):
+    print('and is zero')
+else:
+    print('and is less than 6 and not 0')
