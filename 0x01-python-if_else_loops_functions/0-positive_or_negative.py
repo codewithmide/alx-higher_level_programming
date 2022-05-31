9e32cbe23648045d9448094cb144560527cf4f04
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-05-31 04:09:23 -0700

    added fstring to the program output

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if (number > 0):
-    print('is positive')
+    print(f'{number} is positive')
 elif (number == 0):
-    print('is zero')
+    print(f'{number} is zero')
 else:
-    print('is less than zero')
+    print(f'{number} is less than zero')
