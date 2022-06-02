0c2e3afc46abad623a8a3df2c6c5bc80e61c1df9
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-02 07:53:29 -0700

    formatted my code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,9 +5,6 @@ if __name__ == "__main__":
 if len(argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
-if argv[2] not in ["+", "-", "*", "/"]:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
 a = int(argv[1])
 b = int(argv[3])
 if argv[2] == "+":
@@ -18,3 +15,6 @@ elif argv[2] == "*":
     print('{:d} * {:d} = {:d}'.format(a, b, add(a, b)))
 elif argv[2] == "/":
     print('{:d} / {:d} = {:d)}'.format(a, b, add(a, b)))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
