60624247f537ae3102327f4a2f6638b289575f41
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-02 07:12:39 -0700

    typo error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv, exit
     from calculator_1 import add, sub, mul, div
 if len(argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operaor> <b>")
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
 if argv[2] not in ["+", "-", "*", "/"]:
     print("Unknown operator. Available operators: +, -, * and /")
