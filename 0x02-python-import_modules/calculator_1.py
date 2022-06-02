9dec0e58e740ef4a9dc31aa8b08ab2a342cfa54c
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-02 02:55:58 -0700

    a program that imports functions from the file calculator_1.py, does some Maths, and prints the result.

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a+b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    return(a - b)
+
+
+def mul(a, b):
+    return (a * b)
+
+
+def div(a, b):
+    return int(a / b)
