bd2fcd35e6a2713b53be62079bb468a450052f25
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-02 07:41:23 -0700

    typo error

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,7 +4,7 @@ def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
-            c = add(c, n)
+            c = add(c, i)
         return c
     else:
         return sub(a, b)
