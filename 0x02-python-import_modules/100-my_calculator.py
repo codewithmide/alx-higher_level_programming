9e16be0ae37de41f40cf3dfec59caed3caeb023d
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-02 07:44:35 -0700

    swapped not in to !=

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 if len(argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
-if argv[2] not in ["+", "-", "*", "/"]:
+if argv[2] != ["+", "-", "*", "/"]:
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
 a = int(argv[1])
