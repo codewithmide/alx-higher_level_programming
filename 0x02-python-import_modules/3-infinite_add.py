318f39fdac65bd7dbe23a436e74b3131a55e2cd9
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-02 06:30:58 -0700

    corrected the argument vector

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
+    argc = len(argv)
     result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-        print(result)
+    for i in range(1, argc):
+        result += int(argv[i])
+    print(f'{result:d}')
