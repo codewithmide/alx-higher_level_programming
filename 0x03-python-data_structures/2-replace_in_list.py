80628eb85f605e475e5d27e1cf4beeab5918091c
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-04 03:51:27 -0700

    identation error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
-        return my_list
-    my_list[idx] = element
+        my_list[idx] = element
     return (my_list)
