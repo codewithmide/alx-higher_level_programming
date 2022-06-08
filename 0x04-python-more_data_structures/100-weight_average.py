6327ffde34cccd35f6dbc13b0b857867d2bb5d95
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-08 06:33:54 -0700

    a function that returns the weighted average of all integers tuple (<score>, <weight>)

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def get_ponderated_promedium(scores):
+    list_promediums = list(map(lambda a: a[0] * a[1], scores))
+    return sum(list_promediums)
+def get_sum_weight(scores):
+    list_weights = list(map(lambda a: a[1], scores))
+    return sum(list_weights)
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    else:
+        average = get_ponderated_promedium(my_list) / get_sum_weight(my_list)
+        return average
