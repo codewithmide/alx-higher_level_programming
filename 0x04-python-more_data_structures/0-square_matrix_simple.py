f6d7569badc3b6e727163f7f798e9e7d50066567
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-08 04:32:12 -0700

    a function that prints the power of each number in matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
-#!/usr/bin//python3
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x**2 for x in num] for num in matrix]
-    return new_matrix
+    square = [[x**2 for x in num] for num in matrix]
+    return square
