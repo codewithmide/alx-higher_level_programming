75356a761dd45c99ce02f671e65fbe00d8a62b7f
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-08 05:28:51 -0700

    Roman to Integer Technical interview preperation

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """
+    A function that converts a Roman numeral to an integer.
+    """
+    num_map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    i = 0
+    total = 0
+    if isinstance(roman_string, str):
+        for i in range(len(roman_string) - 1):
+            if num_map[roman_string[i]] >= num_map[roman_string[i + 1]]:
+                total += num_map[roman_string[i]]
+            else:
+                total -= num_map[roman_string[i]
+            i += 1
+            total += num_map[roman_string[i]]
+            return total
+        return 0
