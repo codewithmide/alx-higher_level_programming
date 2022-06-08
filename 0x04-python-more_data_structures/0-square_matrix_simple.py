a11c71fc750bf8adcace4711b52b72c5d8e5115f
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-08 04:21:41 -0700

    get the power of each numbers in matrix:

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 def square_matrix_simple(matrix=[]):
     result = []
     for each_row in matrix:
-        each_matrix = map(lambda num: num ** 2, each_row)
+        each_matrix = map(lambda num: num**2, each_row)
         result.append(list(each_matrix))
     return result
