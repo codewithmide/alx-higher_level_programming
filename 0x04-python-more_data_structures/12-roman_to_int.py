5fe26c50c5d8774dde7d6fd5b309e64a6e4da5b5
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-08 06:26:26 -0700

    concert roman numeral to integer: Technical interview question

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,16 +3,32 @@ def roman_to_int(roman_string):
     """
     A function that converts a Roman numeral to an integer.
     """
-    num_map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    i = 0
-    total = 0
-    if isinstance(roman_string, str):
-        for i in range(len(roman_string) - 1):
-            if num_map[roman_string[i]] >= num_map[roman_string[i + 1]]:
-                total += num_map[roman_string[i]]
-            else:
-                total -= num_map[roman_string[i]
-            i += 1
-            total += num_map[roman_string[i]]
-            return total
+    if not roman_string:
         return 0
+    if not roman_string.isupper():
+        return 0
+    # Roman dictionary
+    Roman_dic = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+    temp = list(roman_string)
+    # Concat 4 and 9s
+    if len(temp) > 1:
+        idx = 0
+        for i in temp:
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'V':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            try:
+                if temp[idx] == 'I' and temp[idx + 1] == 'X':
+                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
+            except IndexError:
+                pass
+            idx + 1
+    # Search in dic for correct number and add
+    for k, v in Roman_dic.items():
+        for index in temp:
+            if index == k:
+                result += v
+    return result
