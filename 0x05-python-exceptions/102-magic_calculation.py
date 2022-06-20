e536ac408488312e3cddcc8b7d99b2cd27891763
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-20 05:00:26 -0700

    whitespace error

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,7 +3,7 @@ def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
-            if i > a:    
+            if i > a:
                 raise Exception('Too far')
             result += a ** b / i
         except Exception:
