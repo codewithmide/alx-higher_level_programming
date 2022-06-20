65f9d132686f5d6b96922b80cdfa35013c1b7b2a
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-20 03:08:08 -0700

    indentation error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 def safe_print_integer(value):
-  try:
-    print("{:d}".format(value))
-  except:
-    return False
-  else:
-    return True
+    try:
+        print("{:d}".format(value))
+    except:
+        return False
+    else:
+        return True
