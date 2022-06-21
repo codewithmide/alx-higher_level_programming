60f0162b611b825ca6b648328c5dd4b88d617d2a
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-21 04:20:14 -0700

    added a blank line

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -22,10 +22,11 @@ class Square:
                 raise ValueError("size must be >= 0")
             else:
                 self.__size = size
+
     def area(self):
         """Calculates the square's area
 
         Returns:
             Area
-         """
+        """
         return (self.__size) ** 2
