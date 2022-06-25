f5364f0548984b3d482ebb165313f490d4f6dbab
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-25 03:26:51 -0700

    say my name baby hehehehehe

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a function that prints My name is <first name> <last name>
+The first name must be a string
+The last name must be a string
+"""
+def say_my_name(first_name, last_name=""):
+    """
+    Defining the function name and arguments to be taken
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
