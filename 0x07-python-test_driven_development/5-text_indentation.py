2bd02b092a2965afd1a512d84f9a55c563085441
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-06-25 07:59:44 -0700

    ARRRRRRRRRHHHHHHHHHHHHH

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A function that prints a text with 2 new lines after each of these characters: ., ? and :
+There should be no space at the beginning or at the end of each printed line
+text must be a string otherwise a typeerror will be raised
+"""
+
+
+def text_indentation(text):
+    """
+    Text indentation function
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for char in ".?:":
+        text = text.replace(char, char + "\n\n")
+
+    list_lines = [lines.strip(' ') for lines in text.split('\n')]
+    revised = "\n".join(list_lines)
+    print(revised, end="")
