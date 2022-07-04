c824849603bb7cc2706c5b1a4fcab499f8bd31ac
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-07-04 05:46:55 -0700

    added the super() function

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,10 @@ Class
 class MyList(list):
     """Class MyList"""
 
+    def __init__(self):
+        """Inherit the mother class"""
+        super().__init__()
+
     def print_sorted(self):
         """prints sorted list""" 
         print(sorted(self))
