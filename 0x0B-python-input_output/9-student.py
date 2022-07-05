a30b0dd627ba7664fc1e90e64147ebe3e04a2e44
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-07-05 07:09:30 -0700

    student json

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes the data"""
+        self.first_name = first_name
+        self.last_name = last_name 
+        self.age = age
+
+    def to_json(self):
+        """Returns a dictionary representation of a Student instance"""
+        return self.__dict__
