1a616e9759ef09841429525a99b7b1ec7df24633
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-07-05 06:06:20 -0700

    deleted end of line

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,5 +5,6 @@
 def class_to_json(obj):
     """Returns the dictionary description with simple data
     structure (list, dictionary, string, integer and boolean)
-    for JSON serialization of an object"""
+    for JSON serialization of an object
+    """
     return obj.__dict__
