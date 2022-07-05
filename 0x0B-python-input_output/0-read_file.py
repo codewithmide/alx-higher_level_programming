d2ed2a0e366828b30e582e7110f34802a6d6bee9
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-07-05 03:38:07 -0700

    a function that writes a string to a text file (UTF8) and returns the number of characters written:

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+A function that reads a text file (UTF8) and prints it to stdout:
+"""
+def read_file(filename=""):
+    """calling the function"""
+    with open(filename, encoding="UTF-8") as readfile:
+        print(readfile.read(), end="")
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A function that writes a string to a text file (UTF8)
+Returns the number of characters written
+"""
+def write_file(filename="", text=""):
+    """
+    Calling the function
+    """
+    with open(filename, mode="w", encoding="UTF8") as writefile:
+        return writefile.write(text)
