b2ec46c009207885bb5b2249097e05dee1528484
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-07-05 05:48:42 -0700

    risky

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Function """
+import sys
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+try:
+    js_list = load_from_json_file("add_item.json")
+except:
+    js_list = []
+
+for arg in sys.argv[1:]:
+    js_list.append(arg)
+save_to_json_file(js_list, "add_item.json")
