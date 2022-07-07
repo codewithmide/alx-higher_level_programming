1cd9dba12d27d451c0860e9963adb4cfafa604b8
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-07-07 05:55:25 -0700

    the class Rectangle by adding validation of all setter methods and instantiation (id excluded):

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,3 +26,72 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+    @property
+    def width(self):
+        """
+        width property getter
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        height property getter
+        """
+        return self.__height
+
+    @property
+    def x(self):
+        """
+        x property getter
+        """
+        return self.__x
+
+    @property
+    def y(self):
+        """
+        y property getter
+        """
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """
+        width setter
+        """
+        if not (isinstance(value, int)):
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """
+        height setter
+        """
+        if not (isinstance(value, int)):
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """
+        x setter
+        """
+        if not (isinstance(value, int)):
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        if not (isinstance(value, int)):
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = value
