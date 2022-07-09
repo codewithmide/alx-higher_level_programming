5a6556560e03c464d41e5125ddbc0e02a3c2fcb4
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-07-09 06:17:44 -0700

    Update the class Base by adding the static method def to_json_string(list_dictionaries): that returns the JSON string representation of list_dictionaries:

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ Class Base:
     private class attribute __nb_objects = 0
     class constructor: def __init__(self, id=None):
 """
+import json
 
 
 class Base:
@@ -16,3 +17,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Update the class Base by adding the static method
+        def to_json_string(list_dictionaries):
+        that returns the JSON string representation of list_dictionaries:
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            string = json.dumps(list_dictionaries)
+            return string
