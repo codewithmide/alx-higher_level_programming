d2fa094a98c5727b0203cff8ef3a465bbdb2bf1b
olúmidé Micheal <olumidemicheal7@gmail.com>
2022-07-21 05:47:34 -0700

    fix

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -22,20 +22,16 @@ class TestBase(unittest.TestCase):
     def test_1_0(self):
         """Create new instances: check for id."""
 
-        b0 = Base()
-        self.assertEqual(b0.id, 1)
         b1 = Base()
-        self.assertEqual(b1.id, 2)
-        b2 = Base(12)
-        self.assertEqual(b2.id, 12)
-        b3 = Base(0)
-        self.assertEqual(b3.id, 0)
-        b4 = Base(927)
-        self.assertEqual(b4.id, 927)
-        b5 = Base(-5)
-        self.assertEqual(b5.id, -5)
-        b6 = Base(9)
-        self.assertEqual(b6.id, 9)
+        self.assertEqual(b1.id)
+        b2 = Base()
+        self.assertEqual(b2.id)
+        b3 = Base()
+        self.assertEqual(b3.id)
+        b4 = Base(12)
+        self.assertEqual(b4.id)
+        b5 = Base()
+        self.assertEqual(b5.id)
 
     def test_1_1(self):
         """Test for type and instance."""
