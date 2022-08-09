761bbcf77b85887fce5507269d80374665ef69e2
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-09 06:54:57 -0700

    remove/delete database

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
+-- If the database hbtn_0c_0 already exists, your script should not fail
+-- You are not allowed to use the SELECT or SHOW statements
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
