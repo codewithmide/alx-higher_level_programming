608eda44eec63bc9cf57fb90e19d677bbbecfc4a
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-09 07:08:49 -0700

    a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,7 @@
+-- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
+-- New row:
+--     id = 89
+--     name = Holberton School
+-- The database name will be passed as an argument of the mysql command
+
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Holberton School');
