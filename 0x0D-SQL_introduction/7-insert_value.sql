019c11cc3d1bd8578dfbb5f5ffcfc6087372e2b7
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-09 07:13:32 -0700

    adjustment

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -4,4 +4,4 @@
 --     name = Holberton School
 -- The database name will be passed as an argument of the mysql command
 
-INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Holberton School');
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
