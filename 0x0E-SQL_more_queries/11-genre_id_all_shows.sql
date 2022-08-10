139f45063b862577f573872d89bd1108798dfc66
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-10 04:30:17 -0700

    a script that lists all shows contained in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
