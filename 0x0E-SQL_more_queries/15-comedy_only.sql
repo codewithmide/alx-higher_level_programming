6a46d12e039e93eadde60a6d89256afc14a44c0a
Olumide Micheal <olumidemicheal7@gmail.com>
2022-08-10 04:39:48 -0700

    deleted whitespace

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,7 +4,6 @@ SELECT g.`name`
   FROM `tv_genres` AS g
        INNER JOIN `tv_show_genres` AS s
        ON g.`id` = s.`genre_id`
-
        INNER JOIN `tv_shows` AS t
        ON t.`id` = s.`show_id`
        WHERE t.`title` = "Dexter"
