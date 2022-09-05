7ac8189945d5f8c9eecdcf206b5419d3c7dcbddf
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-05 13:57:06 -0700

    used indentation

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 3; i++) {
+const myArray = [
+  'C is fun',
+  'Python is cool',
+  'Javascript is amazing'
+];
+for (let i = 0; i < myArray.length; i++) {
   console.log(myArray[i]);
 }
