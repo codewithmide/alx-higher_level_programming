f3890b70fb507281ab20c49a597a3837797625d4
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-05 13:48:38 -0700

    fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0, len = myArray.length; i < len; i++) {
+for (let i = 0; i < myArray.length; i++) {
   console.log(myArray[i]);
 }
