ab1d72a32f43e76bd921616a5f0488e097552216
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-06 06:33:29 -0700

    added an if statement'

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 
