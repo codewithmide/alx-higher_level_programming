19a6966f2fc7343df4579b96bd7b5ac6388b5225
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-09 03:51:53 -0700

    removed the function method

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,29 +3,21 @@ import sys
 import MySQLdb
 
 
-def get_states(username, password, db_name):
+if __name__ == '__main__':
     """
     List all states in the database
     """
     db = MySQLdb.connect(
-            host="localhost",
-            user=username,
-            passwd=password,
-            db=db_name,
+            host='localhost',
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
             port=3306)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
         cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    credentials = sys.argv
-    username = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
-    get_states(username, passwd, db_name)
