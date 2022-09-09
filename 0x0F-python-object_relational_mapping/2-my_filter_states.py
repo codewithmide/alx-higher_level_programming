907172cee5d1d7a7de40937c1c987596df62d39d
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-09 04:32:10 -0700

    used the format method to filter specific values in a database

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db_connection = MySQLdb.connect(host='localhost',
+                                    port=3306,
+                                    user=sys.argv[1],
+                                    passwd=sys.argv[2],
+                                    db=sys.argv[3])
+    cur = db_connection.cursor()
+    cur.execute("""SELECT * FROM states
+            WHERE name LIKE BINARY '{}'
+            ORDER BY id ASC""".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
