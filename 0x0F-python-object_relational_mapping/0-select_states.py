54d6d5131f7cba3bab3270f9d31bb17beb06b1cc
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-09 03:43:58 -0700

    pycodestyle validation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,19 +7,21 @@ def get_states(username, password, db_name):
     """
     List all states in the database
     """
-    db = MySQLdb.connect(host="localhost",
+    db = MySQLdb.connect(
+            host="localhost",
             user=username,
             passwd=password,
             db=db_name,
             port=3306)
-    
+
     cursor = db.cursor()
     cursor.execute("SELECT * FROM `states` ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
+        cursor.close()
     db.close()
+
 
 if __name__ == "__main__":
     credentials = sys.argv
