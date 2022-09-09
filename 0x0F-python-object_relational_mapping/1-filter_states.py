ce4ba15f8c87c356158fbf95fd896de99d8ada86
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-09 04:12:05 -0700

    try

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db_connection = MySQLdb.connect(host='localhost',
+                                    port=3306,
+                                    user=sys.argv[1],
+                                    passwd=sys.argv[2],
+                                    db=sys.argv[3])
+    cur = db_connection.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
