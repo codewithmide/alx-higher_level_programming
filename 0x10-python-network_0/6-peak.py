cda2d3a9cd7738f82675e310d2d0ef69a15a768c
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-23 05:28:31 -0700

    fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""
-    Write a function that finds a peak in a list of unsorted integers.
-"""
 
 
-def find_peak(inArray):
-    """
-    function that finds a peak in a list of unsorted integers.
-    """
-    if not inArray:
+def find_peak(nums):
+    '''
+        Finds the pick in a list of numbers
+    '''
+    length = len(nums)
+    if length == 0:
         return None
-    newArray = []
-    lenArray = len(inArray)
+    if length == 1:
+        return (nums[0])
+    if length == 2:
+        return nums[0] if nums[0] >= nums[1] else nums[1]
 
-    for i in range(1, lenArray-1):
-        newArray.append(inArray[i])
-    searchPeak = newArray[0]
-    for j in range(len(newArray)):
-        if newArray[j] >= searchPeak:
-            searchPeak = newArray[j]
-    return searchPeak
+    for idx in range(0, length):
+        value = nums[idx]
+        if (idx > 0 and idx < length - 1 and
+                nums[idx + 1] <= value and nums[idx - 1] <= value):
+                return value
+        elif idx == 0 and nums[idx + 1] <= value:
+            return value
+        elif idx == length - 1 and nums[idx - 1] <= value:
+            return value
+    return pick
