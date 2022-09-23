183cd7a7afe055454d55b310cbdeae2cbdfa30fb
Olumide Micheal <olumidemicheal7@gmail.com>
2022-09-23 05:31:34 -0700

    used another method

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write Bash script x header variable X-School must be sent with the value 98
-sudo curl -s -H "X-School-User-Id: 98"
+sudo curl -s "$1" -X GET -H "X-School-User-Id: 98"
