37a67b5c251f2cb4b24d5a8647699bb3f1f753e9
Olumide Micheal <olumidemicheal7@gmail.com>
2022-10-18 08:26:02 -0700

    fix ##semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
-	if (err) {
-		return (console.log(err));
-	} else {
-		console.log(data);
-	}
+  if (err) {
+    return (console.log(err));
+  } else {
+    console.log(data);
+  }
 });
