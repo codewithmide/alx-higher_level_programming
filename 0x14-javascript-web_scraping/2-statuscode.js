33cf14cf4acd8557acaf032c41034f21b1f9df0b
Olumide Micheal <olumidemicheal7@gmail.com>
2022-10-18 13:41:18 -0700

    reverted back to the request module

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-const axios = require('axios');
-async function makeHeadRequest () {
-  try {
-    const res = await axios.head(process.argv[2]);
-    console.log(`code: ${res.status}`);
-  } catch (err) {
-    console.log('code:', err.response.status);
-  }
-}
-makeHeadRequest();
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
