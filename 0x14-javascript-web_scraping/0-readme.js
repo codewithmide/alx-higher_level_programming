317654e77a6a501f9017f4f18c1d3d2067f6f979
Olumide Micheal <olumidemicheal7@gmail.com>
2022-10-18 14:33:57 -0700

    fix using the semistandard js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
   if (err) {
-    return (console.log(err))
+    return (console.log(err));
   } else {
-    console.log(data)
+    console.log(data);
   }
-})
+});
