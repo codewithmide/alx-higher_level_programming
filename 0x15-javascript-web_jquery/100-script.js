41598bc8c26edeb602adaf81a0cf91e59a758314
Olumide Micheal <olumidemicheal7@gmail.com>
2022-10-28 11:50:18 +0100

    used the DOMContentLoaded method without using the api

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000):
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('HEADER').style.color = '#FF0000';
+  });
