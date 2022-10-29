12419529928f84ce6f115312d26d673820ccdb1d
Olumide Micheal <olumidemicheal7@gmail.com>
2022-10-29 15:04:27 +0100

    used an api that accepts parameters

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Write a JavaScript script that fetches and prints how to say “Hello” depending on the language
+
+// You should use this API service: https://www.fourtonfish.com/hellosalut/hello/
+// The language code will be the value entered in the tag INPUT#language_code (ex: es, fr, en etc.)
+// The translation must be fetched when the user clicks on INPUT#btn_translate
+// The translation of “Hello” must be displayed in the HTML tag DIV#hello
+// You can’t use document.querySelector to select the HTML tag
+// You must use the JQuery API
+// You script must work when imported from the <head> tag
+
+$('document').ready(function () {
+    let languageCode = $('#language_code');
+    let btn = $('#btn_translate');
+    let translation = $('DIV#hello');
+    let url = 'https://www.fourtonfish.com/hellosalut/?';
+
+    btn.click(function () {
+        $.get(url + $.param({ lang: languageCode.val() }), function (data) {
+            translation.html(data.hello);
+        });
+    });
+});
